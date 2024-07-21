68b64ec3a9103c0155e5509bb20c1295d8122a2f
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-21 23:44:11 +0530

    Variable Types

---
+++ src/class_13072024/Lab015.java
@@ -0,0 +1,24 @@
+package class_13072024;
+
+public class Lab015 {
+    public static void main(String[] args) {
+        boolean is_married = true;
+        boolean is_dog=false;
+
+        System.out.println(is_dog);
+        System.out.println(is_married);
+
+        byte b= 100;
+        short s=10;
+        int age=130;
+        long phone_no=9822304101L;
+        char char_name='a';
+        float pi=3.14f;
+
+        double d=100.1874;
+
+
+    }
+
+
+}
