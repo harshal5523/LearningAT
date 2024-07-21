c8002988d0db6893fda397ca74e319d31418a27c
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-21 17:15:47 +0530

    Shortcuts Commends

---
+++ src/class_05072024/Lab005.java
@@ -0,0 +1,20 @@
+package class_05072024;
+
+public class Lab005 {
+ //shortcut
+ public static void main(String[] args) {
+
+     //Print shortcut
+
+     System.out.println("sout + Space");
+     System.out.println("sout + Space");
+     System.out.println("sout + Space");
+
+
+     //Duplicate above statement
+
+
+ }
+
+
+}
