d39b86a1fd5371e7572a4bfdb747b96be1c2dd3a
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-21 16:58:29 +0530

    Multiline Comments.

---
+++ src/class_05072024/Lab003.java
@@ -0,0 +1,14 @@
+package class_05072024;
+
+public class Lab003 {
+
+    //MultiLine comments
+
+    public static void main(String[] args) {
+        System.out.println("Hello Brther");
+        byte b=10;
+        char a ='a';
+        System.out.println(b+a);
+
+    }
+}
