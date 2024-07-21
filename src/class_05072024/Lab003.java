db4343ca6810d9d43d3f4c9157f982c775f16503
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-21 16:58:39 +0530

    Multiline Comments.

--- src/class_05072024/Lab003.java
+++ src/class_05072024/Lab003.java
@@ -5,10 +5,10 @@ public class Lab003 {
     //MultiLine comments
 
     public static void main(String[] args) {
-        System.out.println("Hello Brther");
-        byte b=10;
-        char a ='a';
-        System.out.println(b+a);
+//        System.out.println("Hello Brther");
+//        byte b=10;
+//        char a ='a';
+//        System.out.println(b+a);
 
     }
 }
