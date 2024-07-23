257ac0a2018824c4a9eed2c8a890a811fcf08af6
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-23 23:57:46 +0530

    Final Keyword

---
+++ src/class_13072024/Lab031.java
@@ -0,0 +1,12 @@
+package class_13072024;
+
+public class Lab031 {
+    public static void main(String[] args) {
+        //Constant we needs to used for final keyword, if used final then value should not be changed.
+
+        final long  mobile_no = 9822304101L;
+        System.out.println(mobile_no);
+        System.out.printf("My Permanat Cell No is %d ", mobile_no);
+
+    }
+}
