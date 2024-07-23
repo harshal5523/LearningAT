adae21f68f84a70b5c1e91d2c5af639874196dff
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-23 23:44:46 +0530

    Calculator Program

---
+++ src/class_13072024/Lab029.java
@@ -0,0 +1,25 @@
+package class_13072024;
+
+public interface Lab029 {
+    public static void main(String[] args) {
+
+        double a=10;
+        double b=34;
+        double sum=a+b;
+        double sub=a-b;
+        double mul=a*b;
+        double div=a/b;
+        double rem=a%b;
+
+        System.out.printf("Addition is %f", sum);
+        System.out.printf("\n");
+        System.out.printf("Sub  is %f",sub);
+        System.out.printf("\n");
+        System.out.printf("Mul is %f",div);
+        System.out.printf("\n");
+        System.out.printf("Div is %f",mul);
+        System.out.printf("\n");
+        System.out.printf("Rem is %f",rem);
+
+    }
+}
