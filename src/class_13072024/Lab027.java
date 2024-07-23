498cfa1e14d2039d3ca8ab2598d7178f1fab77ff
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-23 22:37:44 +0530

    //Formatting

---
+++ src/class_13072024/Lab027.java
@@ -0,0 +1,10 @@
+package class_13072024;
+
+public class Lab027 {
+    public static void main(String[] args) {
+        int age=15;
+        int b=10101;
+
+        //We can store binary no as well.
+    }
+}
