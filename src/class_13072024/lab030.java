055263223c2a3dbb9dbd1ee8d4c6df95a2b4d6ec
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-23 23:48:29 +0530

    Same reference name is not allowed.

---
+++ src/class_13072024/lab030.java
@@ -0,0 +1,12 @@
+package class_13072024;
+
+public class lab030 {
+    public static void main(String[] args) {
+        // Same reference name is not allowed in the
+        int age = 65;
+        double age =65;
+
+        // Same reference name is not allowed.
+
+    }
+}
