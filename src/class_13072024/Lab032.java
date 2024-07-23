f5763c444563b64e66d22cdce517970b13da2ee2
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 00:01:57 +0530

    Final Keyword 2

---
+++ src/class_13072024/Lab032.java
@@ -0,0 +1,8 @@
+package class_13072024;
+
+public class Lab032 {
+    public static void main(String[] args) {
+        final String name="harshal";
+        // name="Prasnant"// error vlaue cannot be changed after adding final.
+    }
+}
