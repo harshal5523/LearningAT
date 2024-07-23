1cea3421157149d9aa4a5c57457b105eaba9112b
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-23 23:28:49 +0530

    Variable Value + Literals.

---
+++ src/class_13072024/Lab028.java
@@ -0,0 +1,20 @@
+package class_13072024;
+
+public class Lab028 {
+    public static void main(String[] args) {
+        //Variable Type Reference Name Variable value or Literal
+
+        int age=127;
+        System.out.println(age);
+        //Int is variable type
+        //age is variable namme
+        //127 is variable value or liternal.
+
+        String s1="harshal";
+        System.out.println(s1);
+        //String is variable type.
+        //s1 is variable name reference name.
+        //harshal is variable value or literal.
+
+    }
+}
