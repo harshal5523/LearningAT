536e2aa42a4e53049ff46c3cf530507318b30f7a
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-23 22:34:29 +0530

    //Formatting of string

---
+++ src/class_13072024/Lab026.java
@@ -0,0 +1,9 @@
+package class_13072024;
+
+public class Lab026 {
+    public static void main(String[] args) {
+        String name="Harshal";
+        System.out.printf("%s",name);
+        //Formatting of string
+    }
+}
