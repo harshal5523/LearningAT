57d70833fc32291adcf5331e8f69e1a0c827ecbc
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-23 22:38:48 +0530

    We can store binary no as well.

--- src/class_13072024/Lab027.java
+++ src/class_13072024/Lab027.java
@@ -3,7 +3,7 @@ package class_13072024;
 public class Lab027 {
     public static void main(String[] args) {
         int age=15;
-        int b=101021;
+        int b=1010421;
 
         //We can store binary no as well.
     }
