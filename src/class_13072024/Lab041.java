0cabac5f1130610c87803d3146880dcaf600811d
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 23:47:38 +0530

    Compound Assignment Operator

---
+++ src/class_13072024/Lab041.java
@@ -0,0 +1,18 @@
+package class_13072024;
+
+public class Lab041 {
+    public static void main(String[] args) {
+        // Compound Assignment Operator
+        int a=10;
+
+        a+=12;
+        System.out.println(a);
+        a-=10;
+        System.out.println(a);
+        a*=2;
+        System.out.println(a);
+      a/=2;
+        System.out.println(a);
+
+    }
+}
