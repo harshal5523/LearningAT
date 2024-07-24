1552e363e53591c70cc4597547f1adb899d3b1dc
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 23:49:32 +0530

    Relational Operator

---
+++ src/class_13072024/Lab042.java
@@ -0,0 +1,14 @@
+package class_13072024;
+
+public class Lab042 {
+    public static void main(String[] args) {
+        //relational Operator
+        // < >  >= <= == !=
+
+        int a =10;
+        int b=20;
+
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
