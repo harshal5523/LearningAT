7d0a64180c3eee91cc91d817c84b4e58b8e6109b
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 22:09:21 +0530

    Variable Literal

---
+++ src/class_13072024/Lab036.java
@@ -0,0 +1,11 @@
+package class_13072024;
+
+public class Lab036 {
+    public static void main(String[] args) {
+        // Literal
+
+        int a=34;
+        System.out.println(a);
+        //34 is literal value
+    }
+}
