61fbf51bb63cb8ff183b8fdc13f76a76ebc945fe
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 22:05:31 +0530

    Escape char

---
+++ src/class_13072024/Lab035.java
@@ -0,0 +1,17 @@
+package class_13072024;
+
+public class Lab035 {
+    public static void main(String[] args) {
+        char c= 'a';
+        System.out.println(c);
+
+       // bye byte =1; // not allowed.
+    char c1= '\t';
+    char c2='\b';
+    char c3='\r';
+
+        System.out.println("Harshal" + c3+"Attarde");
+        System.out.println("Harshal" + c3+"Attarde");
+
+    }
+}
