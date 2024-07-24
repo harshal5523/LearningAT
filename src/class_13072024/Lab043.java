c8824e3706a0441e3650a74c1dc9f211589899b5
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 23:52:29 +0530

    Relational Operator 2

---
+++ src/class_13072024/Lab043.java
@@ -0,0 +1,16 @@
+package class_13072024;
+
+public class Lab043 {
+    public static void main(String[] args) {
+        int age_maitha=30;
+        int age_pramod=34;
+        boolean result = age_pramod>age_maitha;
+        System.out.println(result);
+        boolean result1 = age_pramod<age_maitha;
+        System.out.println(result1);
+    }
+
+
+
+
+}
