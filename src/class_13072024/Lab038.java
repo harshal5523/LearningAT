df7348c1019a2a2ede4287c931c1ccf88e0cd1f2
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 23:18:38 +0530

    Unary Operator - It applies for only 1 Operand

---
+++ src/class_13072024/Lab038.java
@@ -0,0 +1,12 @@
+package class_13072024;
+
+public class Lab038 {
+    public static void main(String[] args) {
+        //Unary Operator
+
+        int age = 65;
+        //It applicable only for 1 oprand
+
+        System.out.println(age);
+    }
+}
