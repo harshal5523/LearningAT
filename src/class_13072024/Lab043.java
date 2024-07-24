4b8befaf27cae6f3fe73cf8c15cca157834ffcfd
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 23:55:07 +0530

    Relational Operator 2

--- src/class_13072024/Lab043.java
+++ src/class_13072024/Lab043.java
@@ -3,11 +3,15 @@ package class_13072024;
 public class Lab043 {
     public static void main(String[] args) {
         int age_maitha=30;
-        int age_pramod=34;
+        int age_pramod=30;
         boolean result = age_pramod>age_maitha;
         System.out.println(result);
         boolean result1 = age_pramod<age_maitha;
         System.out.println(result1);
+
+        boolean result3 = age_pramod>=age_maitha;
+        System.out.println(result3);
+
     }
 
 
