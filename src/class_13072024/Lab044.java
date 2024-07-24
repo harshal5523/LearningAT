61e9887b27ac790c99192552ab6379f888c75ec3
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 23:57:04 +0530

    Relational Operator 3

---
+++ src/class_13072024/Lab044.java
@@ -0,0 +1,12 @@
+package class_13072024;
+
+public class Lab044 {
+    public static void main(String[] args) {
+        int a =10;
+        int b=10;
+
+        boolean c = (10>=10);
+        System.out.println(c);
+        // Here thhey will check 10>10 or 10=10 both are same so it will always true
+    }
+}
