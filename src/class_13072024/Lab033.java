f653e676f8a5d7fb85c65ac75c6858108a1d6353
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 21:52:18 +0530

    Formatting

---
+++ src/class_13072024/Lab033.java
@@ -0,0 +1,12 @@
+package class_13072024;
+
+public class Lab033 {
+    public static void main(String[] args) {
+        // Modules %
+
+        double a=34;
+        double b=10;
+        System.out.println(a%b);
+        System.out.printf(" modules is %f ", a%b);
+    }
+}
