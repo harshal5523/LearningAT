5bf4e89ce2c963309e958b03b0ef485513edfe76
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 23:21:53 +0530

    Unary Operator  2 - It applies for only 1 Operand String case + Oprates as concate.

--- src/class_13072024/Lab039.java
+++ src/class_13072024/Lab039.java
@@ -1,12 +1,12 @@
 package class_13072024;
 
-import java.sql.SQLOutput;
+
 
 public class Lab039 {
     public static void main(String[] args) {
         String first_name="harshal";
         String last_name="attarde";
-        System.out.println(first_name+last_name); // In case of String + Operates as Concate.
+        System.out.println(first_name+last_name); // In case of String + Operates as Concat.
 
         byte b=10;
         byte a=8;
