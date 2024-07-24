8bea6214f184d87b3bf811ece9cfc8af7e9d1809
Heharshala <57935241+Heharshala@users.noreply.github.com>
2024-07-24 22:22:56 +0530

    Assgnment + ArithMatic Operator

---
+++ src/class_13072024/Lab037.java
@@ -0,0 +1,21 @@
+package class_13072024;
+
+public class Lab037 {
+    public static void main(String[] args) {
+        //Operators
+
+        // = assignment operators it is used to give value to literal.
+
+        int age =65;
+        int a=10;
+        int b=23;
+        System.out.println(a+b);
+
+
+        //Arithmatic Operator
+
+        System.out.println(b-a);
+        System.out.println(a*b);
+        System.out.println(b/a);
+    }
+}
